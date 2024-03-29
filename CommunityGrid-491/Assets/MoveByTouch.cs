9c06df0800703fb9ca4f523fb1afc07c9d56a4d0
maxxfreund <78813326+maxxfreund@users.noreply.github.com>
2024-03-28 21:33:26 -0400

    clamped x direction movement based on screen size

--- CommunityGrid-491/Assets/MoveByTouch.cs
+++ CommunityGrid-491/Assets/MoveByTouch.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class MoveByTouch : MonoBehaviour
@@ -11,25 +12,55 @@ public class MoveByTouch : MonoBehaviour
 
     public LeftBtn leftBtn;
     public RightBtn rightBtn;
-    private float screenWidth;
+
+    public Camera cam;
+    public float clampSideDistance;
+    Vector3 startingXPos;
+    private float currXPos;
 
     void Start() {
-        screenWidth = Screen.width;
+        // screenWidth = Screen.width;
+        clampSideDistance = 0.45f;
+        startingXPos = cam.WorldToViewportPoint(transform.position);
+
+        // Debug.Log("Screen width: " + screenWidth);
+        Debug.Log("clampSideDistance: " + clampSideDistance);
+        Debug.Log("startingXPos.x: " + startingXPos.x);
     }
 
     void FixedUpdate() {
+
+        currXPos = FindXPos();
+
         if (LeftBtn.LeftBtnDown){
             Debug.Log("TURN LEFT");
             // car.AddForce(new Vector3(-1.0f * turnSpeed * Time.deltaTime, 0, 0));
-            transform.Translate(Vector3.left * turnSpeed * Time.deltaTime);
+            LeftTurn(currXPos);
         }
         else if (RightBtn.RightBtnDown){
             Debug.Log("TURN RIGHT");
             // car.AddForce(new Vector3(1.0f * turnSpeed * Time.deltaTime, 0, 0));
-            transform.Translate(Vector3.right * turnSpeed * Time.deltaTime);
+            RightTurn(currXPos);
         }
         else{
 
+        }
+    }
+
+    float FindXPos(){
+        Vector3 currentX = cam.WorldToViewportPoint(transform.position);
+        return currentX.x;
+    }
+
+    void LeftTurn(float currentX){
+        if (currentX > startingXPos.x - clampSideDistance){
+            transform.Translate(Time.deltaTime * turnSpeed * Vector3.left);
+        }
+    }
+
+    void RightTurn(float currentX){
+        if (currentX < startingXPos.x + clampSideDistance){
+            transform.Translate(Time.deltaTime * turnSpeed * Vector3.right);
         }
     }
 }
