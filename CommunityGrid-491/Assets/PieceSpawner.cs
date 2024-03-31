def840a50e22544ca51d7db714146c54fa189944
maxxfreund <78813326+maxxfreund@users.noreply.github.com>
2024-03-30 22:26:24 -0400

    Spawn random level pieces

--- CommunityGrid-491/Assets/PieceSpawner.cs
+++ CommunityGrid-491/Assets/PieceSpawner.cs
@@ -4,29 +4,52 @@ using UnityEngine;
 
 public class PieceSpawner : MonoBehaviour
 {
-    public GameObject piece1;
+    public GameObject[] pieces;
     public float spawnRate;
     private float timer = 0;
 
-    // Start is called before the first frame update
+    private int minIndex;
+    private int maxIndex;
+    private int indexPick;
+    private Vector3 piece3offset = new(0, 0, 6);
+
+    /*
+        Initializes min and max indexes for the array of pieces,
+        and spawns initial piece.
+    */
     void Start()
     {
-        SpawnPiece(piece1);
+        minIndex = 0;
+        maxIndex = pieces.Length;
+        indexPick = IndexPicker();
+        SpawnPiece(indexPick);
     }
 
-    // Update is called once per frame
+    /*
+        Spawns a new random piece based on the timer
+    */
     void Update()
     {
         if (timer < spawnRate){
             timer += Time.deltaTime;
         }
         else{
-            SpawnPiece(piece1);
+            indexPick = IndexPicker();
+            SpawnPiece(indexPick);
             timer = 0;
         }
     }
 
-    void SpawnPiece(GameObject piece){
-        Instantiate(piece, transform.position, transform.rotation);
+    void SpawnPiece(int i){
+        if (i == 2){
+            Instantiate(pieces[i], transform.position + piece3offset, transform.rotation);
+        }
+        else{
+            Instantiate(pieces[i], transform.position, transform.rotation);
+        }
+    }
+
+    int IndexPicker(){
+        return Random.Range(minIndex, maxIndex);
     }
 }
